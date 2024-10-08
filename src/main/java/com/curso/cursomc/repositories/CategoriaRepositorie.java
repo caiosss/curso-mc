b50338082e03c6125e6260efb161ff0aa5a0d49d
Caio <caiobarros1712@gmail.com>
2024-10-08 17:49:57 -0300

    Criando Repositorie e Service - Categoria

---
+++ src/main/java/com/curso/cursomc/repositories/CategoriaRepositorie.java
@@ -0,0 +1,9 @@
+package com.curso.cursomc.repositories;
+
+import com.curso.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepositorie extends JpaRepository<Categoria, Integer> {
+}
